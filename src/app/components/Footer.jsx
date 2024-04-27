a71da40a59ecbb8283484265edd074cca01160bd
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2024-04-27 23:27:04 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,8 +5,6 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>INSAAF</span>
-        <p className="text-slate-600">They just don&apos;t make
-Men like me anymore</p>
       </div>
     </footer>
   );
